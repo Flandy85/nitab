421a95731fcbd34344811d614b47c560cc2c1456
Anders Gustavsson <gustavsson09@hotmail.com>
2019-03-01 18:26:29 +0100

    Created start page. Added html structure to footer ad index.php files. Added basic functions into functions.php file for activating theme

--- functions.php
+++ functions.php
@@ -1,4 +1,18 @@
+<?php
 function nitab_script_enqueue() {
 	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/style.css', null, '1.0', 'all' );
 	wp_enqueue_style('googlefonts', '//fonts.googleapis.com/css?family=Roboto:400,500,700');
+
+	// Load Javascript
+	wp_enqueue_script('main', get_template_directory_uri() . '/js/bundle.js', array('jquery'), '1.0.0', true);
+
+	
 }
+/*Add the hooks, function kreditz_script_enqueue*/
+add_action( 'wp_enqueue_scripts', 'nitab_script_enqueue' );
+function nitab_theme_setup() {
+// Enables use of navigation menu.
+	register_nav_menu( 'Navigation menu', 'Website main navigation' );
+}
+add_action('init', 'nitab_theme_setup');
+?>
