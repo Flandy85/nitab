4000e81a757096b1d0db545c2366d364fc52fba0
Anders Gustavsson <gustavsson09@hotmail.com>
2019-03-01 19:50:22 +0100

    Created new partials scss files, add new image, added title to header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="sv">
+<html>
 <head>
 	<meta charset="UTF-8">
   	<meta name="description" content="Norrsidans installation & Teknik AB">
@@ -10,4 +10,13 @@
 	<title><?php wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?> >
+<body <?php body_class(); ?>>
+	<header class="header">
+		<nav class="nav">
+			<a href="<?php echo home_url(); ?>">
+				<!-- <img src="<?php //echo get_template_directory_uri(); ?>/src/images/logo.png" class="nitab-logo" alt="Image of the company Nitab Logo"/> -->
+				<h1 class="header-title">Norrsidans installation & Teknik AB</h1>
+			</a>
+			<div class="nav-menu"><?php wp_nav_menu(array('theme_location'=>'primary')); ?></div>
+		</nav>
+	</header>
---
+++ site/start-page.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Template Name: Start page
+*/
+?> <?php get_header(); ?> <main> <?php
+		if(have_posts()) {
+			while(have_posts()) {
+				
+				the_post();
+				?> <h1>This is the main page</h1> <?php
+			}	
+		}	
+	?> </main>
