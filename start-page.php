2d7d45351dd17731c3c6e7a82c5ad81ab1f59b3a
Anders Gustavsson <gustavsson09@hotmail.com>
2019-03-04 21:14:08 +0100

    Added 2 new images and hover effects on containers

--- start-page.php
+++ start-page.php
@@ -24,13 +24,20 @@
 					<h2><?php echo get_field('company-about-title'); ?></h2>
 				</div>
 				<div class="second-container" id="about">
+					
 					<div class="p1-text-block">
-						<h3><?php echo get_field('company-p1-name'); ?></h3>
-						<p><?php echo get_field('company-p1-text'); ?></p>
+						<img src="<?php echo get_template_directory_uri(); ?>/src/images/fan-wall.jpg"" class="p1-img">
+						<div class="p1-text">
+							<h3><?php echo get_field('company-p1-name'); ?></h3>
+							<p><?php echo get_field('company-p1-text'); ?></p>
+						</div>
 					</div>
 					<div class="p2-text-block">
-						<h3><?php echo get_field('company-p2-name'); ?></h3>
-						<p><?php echo get_field('company-p2-text'); ?></p>
+						<img src="<?php echo get_template_directory_uri(); ?>/src/images/rsz_cutting-wood.jpg"" class="p2-img">
+						<div class="p2-text">
+							<h3><?php echo get_field('company-p2-name'); ?></h3>
+							<p><?php echo get_field('company-p2-text'); ?></p>
+						</div>
 					</div>
 				</div>
 				<div class="third-container-parallax">
